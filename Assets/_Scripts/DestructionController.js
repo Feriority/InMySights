b36cad9619bc743611fcaf6decac327979372d36
Aneesh Goel <aneesh.goel.rbtx@gmail.com>
2016-07-21 15:31:19 -0700

    DestructionController and some exploding barrels

---
+++ Assets/_Scripts/DestructionController.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+var remains: GameObject;
+var destructionEffect: GameObject;
+var HP: int;
+function Update () {
+    // TODO: Proper damage mechanics
+    if(Input.GetMouseButtonDown(0)) {
+        HP -= 1;
+    }
+    if (HP == 0) {
+        if (destructionEffect != null) {
+            Instantiate(destructionEffect, transform.position, transform.rotation);
+        }
+        if (remains != null) {
+            Instantiate(remains, transform.position, transform.rotation);
+        }
+        Destroy(gameObject);
+    }
+}
