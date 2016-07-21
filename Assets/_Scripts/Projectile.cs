cd2a0a2b7fc3fd9eab9eb449e5884fb27da734fa
Aneesh Goel <aneesh.goel.rbtx@gmail.com>
2016-07-21 16:33:16 -0700

    Fix destructible prefabs so meshes match objects

--- Assets/_Scripts/Projectile.cs
+++ Assets/_Scripts/Projectile.cs
@@ -6,6 +6,7 @@ public class Projectile : MonoBehaviour {
 	[HideInInspector] public GameObject target = null;
 	public float maxSpeed;
 	[HideInInspector] public Vector3 velocity;
+    public int damage;
 
 	// Use this for initialization
 	void Start () {
@@ -21,15 +22,14 @@ public class Projectile : MonoBehaviour {
 			transform.position = Vector3.SmoothDamp (transform.position, target.transform.position, ref velocity, time, maxSpeed);
 		}
 	}
-
-    public int damage;
+        
     void OnCollisionEnter(Collision collision) {
         GameObject other = collision.gameObject;
+        // TODO: particle
+        Destroy (gameObject);
         DestructionController controller = other.GetComponent(typeof(DestructionController)) as DestructionController;
         if (controller != null) {
             controller.HP -= damage;
         }
-        // TODO: particle
-		Destroy (gameObject);
 	}
 }
