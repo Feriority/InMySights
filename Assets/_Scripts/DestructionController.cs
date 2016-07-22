c635f4206da48deff722d7984bc4397ad8854ee0
Aneesh Goel <aneesh.goel.rbtx@gmail.com>
2016-07-21 17:18:25 -0700

    Switch to a better explosion fx pack, improve collision and targets for destructibles, make fully destroyed shelf not a target

--- Assets/_Scripts/DestructionController.cs
+++ Assets/_Scripts/DestructionController.cs
@@ -12,12 +12,9 @@ public class DestructionController : MonoBehaviour {
     public int HP;
 	// Update is called once per frame
 	void Update () {
-        // TODO: Proper damage mechanics
-        if(Input.GetMouseButtonDown(0)) {
-            //HP -= 1;
-        }
         if (HP <= 0) {
             if (destructionEffect != null) {
+                // TODO: Clean up explosions
                 Instantiate(destructionEffect, transform.position, transform.rotation);
             }
             if (remains != null) {
